67308451604d117c264cbb453d9305994ca31935
cehayford <sean.alpha18@gmail.com>
2025-01-14 13:05:20 +0000

    add email configuration settings for SMTP in settings.py

--- core/settings.py
+++ core/settings.py
@@ -140,3 +140,12 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Email settings
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.example.com'  
+EMAIL_PORT = 465
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = True
+EMAIL_HOST_USER = 'sngodson@gmail.com'
+EMAIL_HOST_PASSWORD = 'sigvskzfalbmmucx'
