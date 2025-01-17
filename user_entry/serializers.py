344776fe0c0c3b10fcefe6ca55723067cb764d82
cehayford <sean.alpha18@gmail.com>
2025-01-17 06:32:12 +0000

    add depth attribute to serializers for improved nested representation

--- user_entry/serializers.py
+++ user_entry/serializers.py
@@ -5,32 +5,37 @@ class UserinfoSerializer(serializers.ModelSerializer):
     class Meta:
         model = userinfo
         fields = '__all__'
-
+        depth = 1
 
 class ProgLanguageSerializer(serializers.ModelSerializer):
     class Meta:
         model = ProgLanguage
         fields = '__all__'
+        depth = 1
 
 class OtherLanguageSerializer(serializers.ModelSerializer):
     class Meta:
         model = OtherLanguage
         fields = '__all__'
+        depth = 1
 
 class ToolsSerializer(serializers.ModelSerializer):
     class Meta:
         model = Tools
-        fields = '__all__' 
+        fields = '__all__'
+        depth = 1
 
 class DatabasesSerializer(serializers.ModelSerializer):
     class Meta:
         model = Databases
-        fields = '__all__' 
+        fields = '__all__'
+        depth = 1
 
 class FrameworksSerializer(serializers.ModelSerializer):
     class Meta:
         model = Frameworks
         fields = '__all__'
+        depth = 1
 
 class SkillsSerializer(serializers.ModelSerializer):
     class Meta:
@@ -38,8 +43,8 @@ class SkillsSerializer(serializers.ModelSerializer):
         fields = '__all__'
         depth = 1
 
-
 class MedialinksSerializer(serializers.ModelSerializer):
     class Meta:
         model = MediaLinks
         fields = '__all__'
+        depth = 1
