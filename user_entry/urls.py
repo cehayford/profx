99e5a1a93512a1eeb221ae0b827c7795ea6e97d6
cehayford <sean.alpha18@gmail.com>
2025-01-17 03:20:14 +0000

    add URL routing for userinfo and skills views, including sub-urls for programming languages, tools, databases, and frameworks

--- user_entry/urls.py
+++ user_entry/urls.py
@@ -1,7 +1,17 @@
 from django.urls import path
-from .views import CreateMediaLink, UpdateMediaLink, GetMediaLinks
+from .views import CreateMediaLink, UpdateMediaLink, GetMediaLinks, UserinfoView, SkillsView
+from .re_vert import ProgLanguageView, OtherLanguageView, ToolsView, DatabasesView, FrameworksView
 
 urlpatterns = [
+    path('userinfo/', UserinfoView.as_view(), name='userinfo'),
+    path('skills/', SkillsView.as_view(), name='skills'),
+    # sub-urls for skills view
+    path('prog-languages/', ProgLanguageView.as_view(), name='prog_languages'),
+    path('other-languages/', OtherLanguageView.as_view(), name='other_languages'),
+    path('tools/', ToolsView.as_view(), name='tools'),
+    path('databases/', DatabasesView.as_view(), name='databases'),
+    path('frameworks/', FrameworksView.as_view(), name='frameworks'),
+
     path('media-links/', GetMediaLinks.as_view(), name='get_media_links'),
     path('media-links/create/', CreateMediaLink.as_view(), name='create_media_link'),
     path('media-links/update/<int:pk>/', UpdateMediaLink.as_view(), name='update_media_link'),
